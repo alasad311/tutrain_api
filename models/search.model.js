bcc89fcfbffe6aad2610510441a29727526bd271
Mahmoud AlAsad <mahmoud.alasad@csuglobal.edu>
2022-05-11 20:18:11 +0400

    Update search.model.js

--- models/search.model.js
+++ models/search.model.js
@@ -16,8 +16,8 @@ const Search = function(search) {
     this.is_trash = courses.is_trash;
 };
 Search.All = (value,result) => {
-    let query = "(SELECT courses.name AS title, courses.img AS img, courses.rating AS rating FROM courses WHERE (name LIKE '%?%' OR description like '%?%' OR code like '%?%' OR tags like '%?%') limit 0,10) UNION ALL (SELECT users.fullname AS title, users.picture AS img, users.rating AS rating FROM users WHERE (fullname LIKE '%?%' OR tags like '%?%' OR about like '%?%')   limit 0,10)";
-    let values = [value,value,value,value,value,value,value]
+    let query = "(SELECT courses.name AS title, courses.img AS img, courses.rating AS rating FROM courses WHERE (name LIKE ? OR description like ? OR code like ? OR tags like ?) limit 0,10) UNION ALL (SELECT users.fullname AS title, users.picture AS img, users.rating AS rating FROM users WHERE (fullname LIKE ? OR tags like ? OR about like ?)   limit 0,10)";
+    let values = ['%'+value+'%','%'+value+'%','%'+value+'%','%'+value+'%','%'+value+'%','%'+value+'%','%'+value+'%']
     sql.query(query,values, (err, res) => {
         if (err) {
         console.log("error: ", err);
