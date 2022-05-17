aabec13fd8fff9a4319442422d78bc96d4f32857
Mahmoud AlAsad Workstation <mahmoud.alasad@csuglobal.edu>
2022-05-17 13:14:28 +0400

    Update search.model.js

--- models/search.model.js
+++ models/search.model.js
@@ -15,49 +15,49 @@ const Search = function(search) {
     this.updated_by = courses.updated_by;
     this.is_trash = courses.is_trash;
 };
-Search.All = (value,page,result) => {
+Search.All = (value, page, result) => {
     let offset = 0;
-    if(page != 0)
+    if (page != 0)
         offset = page * 10;
-    let query = "(SELECT courses.name AS title, courses.duration AS duration,courses.price AS price, 'Online' AS location,courses.tags,courses.img AS img, courses.rating AS rating, 'course' AS stype FROM courses WHERE (name LIKE ? OR description like ? OR code like ? OR tags like ?) limit ?,10) UNION ALL (SELECT users.fullname AS title, NULL AS duration, NULL AS price,users.wilayat_id AS location, NULL as tags, users.picture AS img, users.rating AS rating, 'user' AS stype FROM users WHERE (fullname LIKE ? OR tags like ? OR about like ?) AND users.type != 'student'  limit ?,10)";
-    let values = ['%'+value+'%','%'+value+'%','%'+value+'%','%'+value+'%',offset,'%'+value+'%','%'+value+'%','%'+value+'%',offset]
-    sql.query(query,values, (err, res) => {
+    let query = "(SELECT courses.name AS title, courses.duration AS duration,courses.price AS price, 'Online' AS location,courses.tags,courses.img AS img, courses.rating AS rating, 'course' AS stype FROM courses WHERE (name LIKE ? OR description like ? OR code like ? OR tags like ?) limit ?,10) UNION ALL (SELECT users.fullname AS title, NULL AS duration, users.hour_price AS price,users.wilayat_id AS location, users.tags, users.picture AS img, users.rating AS rating, 'user' AS stype FROM users WHERE (fullname LIKE ? OR tags like ? OR about like ?) AND users.type != 'student'  limit ?,10)";
+    let values = ['%' + value + '%', '%' + value + '%', '%' + value + '%', '%' + value + '%', offset, '%' + value + '%', '%' + value + '%', '%' + value + '%', offset]
+    sql.query(query, values, (err, res) => {
         if (err) {
-        console.log("error: ", err);
-        result(null, err);
-        return;
+            console.log("error: ", err);
+            result(null, err);
+            return;
         }
         console.log("users: ", res);
         result(null, res);
     });
 };
-Search.User = (value,page,result) => {
+Search.User = (value, page, result) => {
     let offset = 0;
-    if(page != 0)
+    if (page != 0)
         offset = page * 10;
-    let query = "SELECT users.fullname AS title, NULL AS duration, NULL AS price,users.wilayat_id AS location, NULL as tags,users.picture AS img, users.rating AS rating, 'user' AS stype FROM users WHERE (fullname LIKE ? OR tags like ? OR about like ?) AND users.type != 'student' limit ?,10";
-    let values = ['%'+value+'%','%'+value+'%','%'+value+'%',offset]
-    sql.query(query,values, (err, res) => {
+    let query = "SELECT users.fullname AS title, NULL AS duration,users.hour_price AS price, NULL AS price,users.wilayat_id AS location, users.tags,users.picture AS img, users.rating AS rating, 'user' AS stype FROM users WHERE (fullname LIKE ? OR tags like ? OR about like ?) AND users.type != 'student' limit ?,10";
+    let values = ['%' + value + '%', '%' + value + '%', '%' + value + '%', offset]
+    sql.query(query, values, (err, res) => {
         if (err) {
-        console.log("error: ", err);
-        result(null, err);
-        return;
+            console.log("error: ", err);
+            result(null, err);
+            return;
         }
         console.log("users: ", res);
         result(null, res);
     });
 };
-Search.Courses = (value,page,result) => {
+Search.Courses = (value, page, result) => {
     let offset = 0;
-    if(page != 0)
+    if (page != 0)
         offset = page * 10;
     let query = "SELECT courses.name AS title, courses.duration AS duration,courses.price AS price, 'Online' AS location,courses.tags, courses.img AS img, courses.rating AS rating, 'course' AS stype FROM courses WHERE (name LIKE ? OR description like ? OR code like ? OR tags like ?) limit ?,10";
-    let values = ['%'+value+'%','%'+value+'%','%'+value+'%','%'+value+'%',offset]
-    sql.query(query,values, (err, res) => {
+    let values = ['%' + value + '%', '%' + value + '%', '%' + value + '%', '%' + value + '%', offset]
+    sql.query(query, values, (err, res) => {
         if (err) {
-        console.log("error: ", err);
-        result(null, err);
-        return;
+            console.log("error: ", err);
+            result(null, err);
+            return;
         }
         console.log("users: ", res);
         result(null, res);
