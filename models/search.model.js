8928e6fb33683f685c60e653e753b02e29395375
Mahmoud AlAsad <mahmoud.alasad@csuglobal.edu>
2022-05-24 20:46:53 +0400

    locaility

--- models/search.model.js
+++ models/search.model.js
@@ -19,7 +19,7 @@ Search.All = (value, page, result) => {
     let offset = 0;
     if (page != 0)
         offset = page * 10;
-    let query = "(SELECT courses.id AS id,courses.name AS title, courses.duration AS duration,courses.price AS price, 'Online' AS location,courses.tags,courses.img AS img, courses.rating AS rating, 'course' AS stype FROM courses WHERE (name LIKE ? OR description like ? OR code like ? OR tags like ?) limit ?,10) UNION ALL (SELECT users.user_id AS id,users.fullname AS title, NULL AS duration, users.hour_price AS price,users.wilayat_id AS location, users.tags, users.picture AS img, users.rating AS rating, 'user' AS stype FROM users WHERE (fullname LIKE ? OR tags like ? OR about like ?) AND users.type != 'student'  limit ?,10)";
+    let query = "(SELECT courses.id AS id,courses.name AS title, courses.duration AS duration,courses.price AS price, 'Online' AS location,courses.tags,courses.img AS img, courses.rating AS rating, 'course' AS stype FROM courses WHERE (name LIKE ? OR description like ? OR code like ? OR tags like ?) limit ?,10) UNION ALL (SELECT users.user_id AS id,users.fullname AS title, NULL AS duration, users.hour_price AS price,CONCAT(users.wilayat,',',users.locality) AS location, users.tags, users.picture AS img, users.rating AS rating, 'user' AS stype FROM users WHERE (fullname LIKE ? OR tags like ? OR about like ?) AND users.type != 'student'  limit ?,10)";
     let values = ['%' + value + '%', '%' + value + '%', '%' + value + '%', '%' + value + '%', offset, '%' + value + '%', '%' + value + '%', '%' + value + '%', offset]
     sql.query(query, values, (err, res) => {
         if (err) {
@@ -35,7 +35,7 @@ Search.User = (value, page, result) => {
     let offset = 0;
     if (page != 0)
         offset = page * 10;
-    let query = "SELECT users.user_id AS id, users.fullname AS title, NULL AS duration,users.hour_price AS price,users.wilayat_id AS location, users.tags,users.picture AS img, users.rating AS rating, 'user' AS stype FROM users WHERE (fullname LIKE ? OR tags like ? OR about like ?) AND users.type != 'student' limit ?,10";
+    let query = "SELECT users.user_id AS id, users.fullname AS title, NULL AS duration,users.hour_price AS price,CONCAT(users.wilayat,',',users.locality) AS location, users.tags,users.picture AS img, users.rating AS rating, 'user' AS stype FROM users WHERE (fullname LIKE ? OR tags like ? OR about like ?) AND users.type != 'student' limit ?,10";
     let values = ['%' + value + '%', '%' + value + '%', '%' + value + '%', offset]
     sql.query(query, values, (err, res) => {
         if (err) {
