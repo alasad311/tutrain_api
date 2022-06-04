893e6586598e8063e5e888c3b9fa1ca3b7648d85
Mahmoud AlAsad <mahmoud.alasad@csuglobal.edu>
2022-06-04 11:32:36 +0400

    Update report.model.js

--- models/report.model.js
+++ models/report.model.js
@@ -4,8 +4,6 @@ const Reports = function(report) {
     this.user_id = report.user_id;
     this.tutor_id = report.tutor_id;
     this.feedback = report.feedback;
-    this.is_open = report.is_open;
-    this.date_created = report.date_created;
 };
 Reports.createReport = (newOrder, result) => {
     sql.query("INSERT INTO reports SET ?", newOrder, (err, res) => {
