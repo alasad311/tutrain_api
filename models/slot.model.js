6481d9d38374a0743b210a991c72202eee90f659
Mahmoud AlAsad <mahmoud.alasad@csuglobal.edu>
2022-06-06 22:45:31 +0400

    Update slot.model.js

--- models/slot.model.js
+++ models/slot.model.js
@@ -9,6 +9,16 @@ const Slots = function(slot) {
     this.duration = slot.duration;
 };
 Slots.createSlot = (newSlot, result) => {
+    sql.query("SELECT * FORM temp_booking WHERE user_id = ? AND tutor_id = ? AND slot = ? AND timefrom = ? AND timeto = ? AND duration = ? ",[newSlot.user_id,newSlot.tutor_id,newSlot.slot,newSlot.timefrom,newSlot.timeto,newSlot.duration], (err, res) => {
+        if (err) {
+        console.log("error: ", err);
+        result(null, err);
+        return;
+        }
+        console.log("users: ", res);
+        result(null, res);
+    });
+
     sql.query("INSERT INTO temp_booking SET ?", newSlot, (err, res) => {
         if (err) {
             console.log("error: ", err);
