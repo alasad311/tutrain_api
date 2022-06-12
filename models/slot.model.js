11d5c019c1ebc5f8e2c9e961cbdf028af5ec337c
Mahmoud AlAsad <mahmoud.alasad@csuglobal.edu>
2022-06-12 22:09:16 +0400

    Update slot.model.js

--- models/slot.model.js
+++ models/slot.model.js
@@ -106,7 +106,7 @@ Slots.updateSlot = (accpeted, id, result) => {
                             data: {
                                 type: "SESSIONRESPONSE",
                                 accpeted: status,
-                                tutorDetails: ""+res[0],
+                                tutorDetails: ""+JSON.stringify(res[0]),
                                 bookID: "" + data[0]['id'],
                                 userFullName: "" + userFullname,
                                 slotDate: "" + data[0]['slot'],
