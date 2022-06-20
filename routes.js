309b8edbe0f8e87ab5bf128a46f6a4325eb828a4
Mahmoud AlAsad <mahmoud.alasad@csuglobal.edu>
2022-06-20 22:22:03 +0400

    Update routes.js

--- routes.js
+++ routes.js
@@ -54,6 +54,7 @@ module.exports = app => {
 
     //Booking Personal Tutor
     router.post("/slot", slot.createSlot)
+    router.post("/slot/cancel", slot.cancelSlot)
     router.post("/slot/update", slot.updateSlot)
     app.use('/api/v1/', router);
 };
