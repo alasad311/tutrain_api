1aa368cd7323571418d92b567fd75894aecc0d53
Mahmoud AlAsad <mahmoud.alasad@csuglobal.edu>
2022-06-20 20:04:11 +0400

    Update order.model.js

--- models/order.model.js
+++ models/order.model.js
@@ -69,7 +69,7 @@ Orders.createOrder = (newOrder, result) => {
                                 data: {
                                     type: "NEWORDER",
                                     userName: "" + userFullname,
-                                    slotDate: "" + res[0].fullsot
+                                    slotDate: "" + res[0].fullslot
 
                                 }
                             }
