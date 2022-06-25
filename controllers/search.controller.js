afde142321514e7bbd83b066206f68836e61d43c
Mahmoud AlAsad <mahmoud.alasad@csuglobal.edu>
2022-06-25 11:19:52 +0400

    session

--- controllers/search.controller.js
+++ controllers/search.controller.js
@@ -70,6 +70,7 @@ exports.searchCourses = (req, res) => {
 exports.searchSessions = (req, res) => {
   const authHeader = req.headers['authorization']
   const token = authHeader && authHeader.split(' ')[1]  
+  const userID = req.headers['userid']
 
   if(token != "09f26e402586e2faa8da4c98a35f1b20d6b033c6097befa8be3486a829587fe2f90a832bd3ff9d42710a4da095a2ce285b009f0c3730cd9b8e1af3eb84df6611")
   {
@@ -78,7 +79,7 @@ exports.searchSessions = (req, res) => {
         });
         return;
   }
-  Search.searchSessions(req.params.value,req.params.page,(err, data) => {
+  Search.searchSessions(req.params.value,userID,req.params.page,(err, data) => {
     if (err)
       res.status(200).send({
         code:err.code,
