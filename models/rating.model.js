584d0db13e4889763b3e607dc2011ff5313aaf6f
Mahmoud AlAsad <mahmoud.alasad@csuglobal.edu>
2022-06-25 14:48:51 +0400

    Update rating.model.js

--- models/rating.model.js
+++ models/rating.model.js
@@ -1,12 +1,11 @@
 const sql = require("./db.js");
-const admin = require('firebase-admin');
-const messaging = admin.messaging()
+
 const Ratings = function(rating) {
     this.user_id = rating.user_id;
     this.order_id = rating.order_id;
     this.value = rating.value;
 };
-Ratings.createOrder = (newRating, result) => {
+Ratings.createRating = (newRating, result) => {
     sql.query("INSERT INTO rating SET ?", newRating, (err, res) => {
         if (err) {
             console.log("error: ", err);
