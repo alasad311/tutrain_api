60c68b6fad8893099c86aef9d054ad1252186372
Mahmoud AlAsad <mahmoud.alasad@csuglobal.edu>
2022-07-14 13:08:43 +0400

    Update courses.model.js

--- models/courses.model.js
+++ models/courses.model.js
@@ -73,7 +73,7 @@ Courses.fetchAllNewCoursesPaged = (id,page,result) => {
     let offset = 0;
     if (page != 0)
         offset = page * 10;
-    let query = "SELECT *  FROM courses LEFT JOIN users ON users.user_id = courses.user_id where courses.is_confirmed = 1 AND users.user_id != ? AND courses.is_trash != 1 ORDER BY id DESC LIMIT ?,5";
+    let query = "SELECT *  FROM courses LEFT JOIN users ON users.user_id = courses.user_id where courses.is_confirmed = 1 AND users.user_id != ? AND courses.is_trash != 1 ORDER BY id DESC LIMIT ?,10";
     sql.query(query,[id,page], (err, res) => {
         if (err) {
         console.log("error: ", err);
