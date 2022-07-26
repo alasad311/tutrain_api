82258f60776cf68a3a4d7d56cc9eed257a749a4e
Mahmoud AlAsad Workstation <mahmoud.alasad@csuglobal.edu>
2022-07-26 13:22:55 +0400

    Update session.model.js

--- models/session.model.js
+++ models/session.model.js
@@ -83,13 +83,13 @@ Session.sessionUpdate = (image, id, body, result) => {
         details += ",location = '" + body.sessionlocation + "'";
     }
     if (body.sessiondescription) {
-        details += ",description = '" + sanitizer.escape(body.sessiondescription).replace("'", '') + "'";
+        details += ",description = '" + sanitizer.sanitize(body.sessiondescription.replace("'", '')) + "'";
     }
     if (image) {
         details += ",img = 'https://tapp.scd.edu.om/uploads/" + image + "'";
     }
     if (body.sessiontag) {
-        details += ",tags = '" + sanitizer.escape(body.sessiontag).replace("'", '') + "'";
+        details += ",tags = '" + sanitizer.sanitize(body.sessiontag.replace("'", '')) + "'";
     }
     if (body.sessionduration) {
         details += ",duration = '" + body.sessionduration + "'";
